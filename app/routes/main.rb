665f88493da38b6ad1a085b0b4bae057d0a22fd9
Adam Edgett <AdamEdgett@gmail.com>
2015-02-06 12:44:17 -0500

    Add json endpoints

--- app/routes/main.rb
+++ app/routes/main.rb
@@ -1,4 +1,5 @@
 require_relative 'helpers'
+require 'json'
 
 # Includes routes for main app functionality
 class App < Sinatra::Base
@@ -11,8 +12,18 @@ class App < Sinatra::Base
     haml :album_list, locals: { albums: albums }
   end
 
+  get '/albums.json' do
+    albums = get_directories(ALBUM_DIR)
+    albums.to_json
+  end
+
   get '/albums/:album' do
     photos = get_images(ALBUM_DIR + "/#{params[:album]}")
     haml :album, locals: { photos: photos }
+  end
+
+  get '/albums/:album/json' do
+    photos = get_images(ALBUM_DIR + "/#{params[:album]}")
+    photos.to_json
   end
 end
