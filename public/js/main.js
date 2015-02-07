8cd12199b4bcc7603e716f2aa3e2fc0a2648145d
Adam Edgett <AdamEdgett@gmail.com>
2015-02-06 21:54:25 -0500

    Update masonry initialization

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,10 @@
-var container = document.querySelector('#js-masonry');
-var masonry;
-imagesLoaded( container, function() {
-  masonry = new Masonry( container, {
-    columnWidth: 0,
-    gutter: 0,
-    itemSelector: '.photo-box'
+var container = document.querySelector('#photo-group');
+if (container) {
+  imagesLoaded( container, function() {
+    new Masonry( container, {
+      columnWidth: 0,
+      gutter: 0,
+      itemSelector: '.photo-box'
+    });
   });
-});
+}
