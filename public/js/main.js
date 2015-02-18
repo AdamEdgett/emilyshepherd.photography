c9cbcea355e7d06da3ab0749e41f1da6566b5e89
Adam Edgett <AdamEdgett@gmail.com>
2015-02-17 21:06:00 -0500

    Add loading spinner to album page

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,10 @@
-var container = document.querySelector('#photo-group');
+var spinnerContainer = document.getElementById('spinner-container');
+var spinner = new Spinner({top: '100px'}).spin(spinnerContainer);
+
+var container = document.getElementById('photo-group');
 if (container) {
   imagesLoaded( container, function() {
+    spinnerContainer.className += 'hidden';
     new Masonry( container, {
       columnWidth: 0,
       gutter: 0,
